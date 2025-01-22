96d22b9bf0ad6eee192e5f53c639b75155decd8f
Tom Waterman <tjwaterman99@gmail.com>
2025-01-22 08:05:57 +0100

    Convert `Gradebook` to a SQLAlchemy model (#10)

--- otter/models.py
+++ otter/models.py
@@ -1,11 +1,16 @@
+import io
 from typing import Optional
 from pathlib import Path
 from dataclasses import dataclass
 from openpyxl.workbook.workbook import Workbook
 from openpyxl import load_workbook
+from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
+from sqlalchemy import Column, BINARY, String, JSON, Integer
 from otter.clients.llama_parse_client import LlamaParseClient
 
 
+class Base(DeclarativeBase):
+    pass
 
 @dataclass
 class GradebookEntry:
@@ -15,18 +20,23 @@ class GradebookEntry:
     section: str
 
 
-class Gradebook:
+class Gradebook(Base):
+
+    __tablename__ = "gradebooks"
+    id = mapped_column(Integer, primary_key=True)
+    original_path: Mapped[str]
+    workbook_bytes: Mapped[bytes] = mapped_column(BINARY)
+    parsed: Mapped[Optional[dict]] = mapped_column(JSON)
 
     read_only = True
-    
-    def __init__(self, path: Path, workbook: Optional[Workbook] = None, parsed: Optional[dict] = None):
-        self.path = path
-        self.workbook = workbook
-        self.parsed = parsed
 
-    def load(self) -> Workbook:
-        self.workbook = load_workbook(self.path, read_only=self.read_only)
-        return self.workbook
+    @property
+    def workbook(self):
+        return load_workbook(io.BytesIO(self.workbook_bytes))
+
+    @property
+    def path(self):
+        return Path(self.original_path)
 
     def parse(self) -> dict:
         client = LlamaParseClient()
--- tests/test_gradebook.py
+++ tests/test_gradebook.py
@@ -1,9 +1,6 @@
-from pathlib import Path
 from otter.models import Gradebook
 
 
-def test_gradebook_loads():
-    gb = Gradebook(path=Path('./tests/fixtures/mock-gradebook.xlsx'))
-    assert gb.path.exists()
-    gb.load()
-    assert 'Unit1' in gb.workbook.sheetnames
+def test_gradebook_loads(gradebook: Gradebook):
+    assert gradebook.path.exists()
+    assert 'Unit1' in gradebook.workbook.sheetnames
