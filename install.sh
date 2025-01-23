63d2be8f9439fe0ad9a94a0f8ce24234c8834748
Tom Waterman <twaterman@coveo.com>
2025-01-23 16:56:57 +0100

    Install even in the system version...

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 echo "Upgrading pip..."
 /usr/bin/python3 -m pip install --upgrade pip
+/usr/bin/python3 -m pip install packaging
 curl --silent https://raw.githubusercontent.com/tjwaterman99/otter/refs/heads/main/start.py | /usr/bin/python3
