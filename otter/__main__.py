b7429b22c4a7176fe4e276e963be314ba9d83826
Tom Waterman <twaterman@coveo.com>
2025-01-23 11:58:02 +0100

    Add delay

--- otter/__main__.py
+++ otter/__main__.py
@@ -1,3 +1,4 @@
+from time import sleep
 from otter import app
 from uvicorn import run
 from threading import Thread
@@ -9,6 +10,11 @@ def start():
 
 
 Thread(target=start, daemon=True).start()
+
+print("Waiting 3 seconds for webserver to start...")
+sleep(3)
+
+print("Opening app")
 webbrowser.open(url="http://127.0.0.1:8000")
 
 while True:
